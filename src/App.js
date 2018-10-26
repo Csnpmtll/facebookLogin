b4702370bfd002037e3d768807a415c28a92ce9c
Csnpmtll <42668985+Csnpmtll@users.noreply.github.com>
2018-10-26 16:52:53 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Register from './Register';
+class App extends Component {
+  render() {
+    return (
+      <Register />
+    );
+  }
+}
+
+export default App;
